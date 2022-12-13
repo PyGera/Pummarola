130933df0fbe7a9119c609252a5057efee85903f
Federico Gerardi <federico.gerardi03@gmail.com>
2022-12-13 14:57:41 +0100

    Initial Commit

---
+++ Pummarola/PummarolaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PummarolaApp.swift
+//  Pummarola
+//
+//  Created by Federico Gerardi on 13/12/22.
+//
+
+import SwiftUI
+
+@main
+struct PummarolaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
